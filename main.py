009d4d5e3c0a7acb24f541cc8d716483df6d9d18
sajjadahmadish <sajjad.ahmadi.sh@gmail.com>
2023-01-17 03:27:44 +0330

    'echo'

--- main.py
+++ main.py
@@ -8,4 +8,11 @@ bot = telebot.TeleBot(constants.API_KEY)
 def start(message):
     bot.reply_to(message, 'Hello, World!')
 
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
 bot.polling()
