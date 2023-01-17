8d8e227c666bb1df6e361e707be0a464e9e38ae8
sajjadahmadish <sajjad.ahmadi.sh@gmail.com>
2023-01-17 03:46:06 +0330

    test set command

--- main.py
+++ main.py
@@ -15,4 +15,21 @@ def echo_message(message):
     bot.reply_to(message, message.text)
 
 
+# use in for delete with the necessary scope and language_code if necessary
+bot.delete_my_commands(scope=None, language_code=None)
+
+bot.set_my_commands(
+    commands=[
+        telebot.types.BotCommand("command1", "command1 description"),
+        telebot.types.BotCommand("command2", "command2 description")
+    ],
+    # scope=telebot.types.BotCommandScopeChat(12345678)  # use for personal command for users
+    # scope=telebot.types.BotCommandScopeAllPrivateChats()  # use for all private chats
+)
+
+# check command
+cmd = bot.get_my_commands(scope=None, language_code=None)
+print([c.to_json() for c in cmd])
+
+
 bot.polling()
